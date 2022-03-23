593ac39b1f266f89ef3bc35f66ad2a7596e3529a
Araceli Arinez <andrea.arinez@ucb.edu.bo>
2022-03-23 16:47:03 -0400

    Añadido Juego 2

--- Juego1.cpp
+++ Juego1.cpp
@@ -147,15 +147,122 @@ public:
     }
 };
 
+class Juego2
+{
+public:
+    void instrucciones()
+    {
+        cout << "En Mastermind compiten 2 jugadores, el jugador que introduce el codigo y el jugador que lo intenta acertar. El jugador que hace el codigo coloca en secreto su combinacion compuesta por 4 numeros. El usuario para acertar la combinacion puede usar 4 numeros (del 1 al 4) que se pueden repetir, ademas saltara un aviso si alguno de los numeros introducidos no corresponde al abanico de numeros mencionado anteriormente. La persona que tiene que descifrar el codigo lo colocara a la espera de que el otro jugador le empiece a dar pistas. Para que el juego no sea imposible se le iran dando pistas: si el hueco en el que el usuario ha anadido el numero coincide en numero y posicion se le avisara y por cada numero acertado pero en un lugar equivocado de la secuencia tambien. El usuario tiene diez intentos para acertar el codigo y en el momento que gane saltara un mensaje diciendo ENHORABUENA HAS ACERTADO EL CODIGO! sino le saltara un mensaje diciendo LO SIENTO NO HAS CONSEGUIDO ADIVINAR EL CODIGO, INTENTA OTRA VEZ!" << endl;
+    }
+
+    void iniciar()
+    {
+        system("cls");
+        int i;
+        int datos[4];
+        for (i = 0; i < 5; i++)
+        {
+            cout << "Estas en el intento " << (i + 1) << " por favor introduce cuatro numero del 1 al 4" << endl;
+            cout << "Primer digito del codigo" << endl;
+            cin >> datos[0];
+            cout << "Segundo digito del codigo" << endl;
+            cin >> datos[1];
+            cout << "Tercer digito del codigo" << endl;
+            cin >> datos[2];
+            cout << "Cuarto digito del codigo" << endl;
+            cin >> datos[3];
+
+            if (datos[0] < 1 || datos[0] > 4)
+            {
+                cout << "Has anadido un numero que no debes anadir, ten cuidado" << endl;
+                cout << endl
+                     << endl;
+            }
+            if (datos[1] < 1 || datos[1] > 4)
+            {
+                cout << "Has anadido un numero que no debes anadir, ten cuidado" << endl;
+                cout << endl
+                     << endl;
+            }
+            if (datos[2] < 1 || datos[2] > 4)
+            {
+                cout << "Has anadido un numero que no debes anadir, ten cuidado" << endl;
+                cout << endl
+                     << endl;
+            }
+            if (datos[3] < 1 || datos[3] > 4)
+            {
+                cout << "Has anadido un numero que no debes anadir, ten cuidado" << endl;
+                cout << endl
+                     << endl;
+            }
+
+            cout << "Tu numero es el ------>" << datos[0] << datos[1] << datos[2] << datos[3] << "<-----------" << endl;
+            int correcto = 0;
+            int aproximacion = 0;
+
+            if (datos[0] == 1)
+            {
+                correcto++;
+            }
+            if (datos[1] == 2)
+            {
+                correcto++;
+            }
+            if (datos[2] == 4)
+            {
+                correcto++;
+            }
+            if (datos[3] == 4)
+            {
+                correcto++;
+            }
+            // Condicion
+            if (datos[0] == 2 || datos[0] == 4)
+            {
+                aproximacion++;
+            }
+            if (datos[1] == 1 || datos[1] == 4)
+            {
+                aproximacion++;
+            }
+            if (datos[2] == 2 || datos[2] == 1)
+            {
+                aproximacion++;
+            }
+            if (datos[3] == 2 || datos[3] == 1)
+            {
+                aproximacion++;
+            }
+
+            cout << "Haz acertado " << correcto << " numeros en la posicion correcta" << endl;
+            cout << "    " << endl;
+            cout << "Haz acertado " << aproximacion << " numeros pero no en la posicion correcta" << endl;
+
+            if (datos[0] == 1 && datos[1] == 2 && datos[2] == 4 && datos[3] == 4)
+            {
+                system("cls");
+                cout << "ENHORABUENA HAS ACERTADO EL CODIGO!" << endl;
+                exit(0);
+            }
+            cout << endl
+                 << endl;
+        }
+
+        cout << "LO SIENTO NO HAS CONSEGUIDO ADIVINAR EL CODIGO,INTENTA OTRA VEZ!";
+        }
+};
 void menu()
 {
     int opcion = -1;
     Juego1 play1;
+    Juego2 play2;
     while (opcion != 0)
     {
 
-        cout << "Menu\n";
+        cout << "\n\nMenu\n";
         cout << "1. Piedra papel o tijera\n";
+        cout << "2. Mastermind\n";
         cout << "0. Salir\n";
         cin >> opcion;
         switch (opcion)
@@ -163,7 +270,10 @@ void menu()
         case 1:
             play1.iniciar();
             break;
-
+        case 2:
+            play2.instrucciones();
+            play2.iniciar();
+            break;
         case 0:
             exit(0);
             break;
---
+++ readme.md
@@ -0,0 +1,8 @@
+# Practica Programacion Superior
+### Grupo 07
+
+Integrantes:
+- Araceli Ariñez
+- Sofia Gonzalez
+- Rodrigo Zabaleta
+- Fabricio Rocabado
